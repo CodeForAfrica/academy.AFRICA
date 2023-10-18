52f21ec136e0522ef90791defbc68fec1e0ed7cd
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-10-18 17:50:26 +0300

    render dynamic menus
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- wp-content/plugins/academy-africa/includes/widgets/header.php
+++ wp-content/plugins/academy-africa/includes/widgets/header.php
@@ -33,12 +33,34 @@ class Academy_Africa_Header extends \Elementor\Widget_Base
         return ['header', 'navigation'];
     }
 
+
+    function get_menus()
+    {
+        $menus = wp_get_nav_menus();
+        $menu_names = [];
+        foreach ($menus as $menu) {
+            $menu_names[$menu->name] = $menu->name;
+        }
+        return $menu_names;
+    }
+
+
     protected function register_controls()
     {
         $this->start_controls_section(
             'section_header',
             [
                 'label' => __('Header', 'academy-africa'),
+            ]
+        );
+        // Select menu
+        $this->add_control(
+            'select_menu',
+            [
+                'label' => __('Select Menu', 'academy-africa'),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'default' => 'primary',
+                'options' => $this->get_menus(),
             ]
         );
 
@@ -64,9 +86,6 @@ class Academy_Africa_Header extends \Elementor\Widget_Base
             ]
         );
 
-
-
-
         $this->end_controls_section();
     }
 
@@ -82,11 +101,35 @@ class Academy_Africa_Header extends \Elementor\Widget_Base
         return $image[0];
     }
 
+    function get_menu_items($name)
+    {
+        $list = wp_get_nav_menu_items($name);
+        $menu_items = [];
+        foreach ($list as $item) {
+            $menu_items[] = [
+                'title' => $item->title,
+                'url' => $item->url,
+                'id' => $item->ID,
+                'parent_id' => $item->menu_item_parent,
+            ];
+        }
+        $menu = [];
+        foreach ($menu_items as $item) {
+            if ($item['parent_id'] == 0) {
+                $menu[$item['id']] = $item;
+            } else {
+                $menu[$item['parent_id']]['children'][] = $item;
+            }
+        }
+        return $menu;
+    }
+
     protected function render()
     {
         $settings = $this->get_settings_for_display();
+        $selected_menu = $settings['select_menu'];
+        $menus = $selected_menu ? $this->get_menu_items($selected_menu) : [];
 
-        // inline editing
         $this->add_inline_editing_attributes('sign_in_button_text', 'none');
 ?>
         <nav class="nav">
@@ -104,19 +147,14 @@ class Academy_Africa_Header extends \Elementor\Widget_Base
                     </button>
                 </div>
                 <div class="navigation-links">
-                    <a href="#" class="nav-link">
-                        <p class="nav-link-text">Courses</p>
-                    </a>
-                    <a href="#" class="nav-link">
-                        <p class="nav-link-text">Events</p>
-                    </a>
-                    <a href="#" class="nav-link">
-                        <p class="nav-link-text">Blog</p>
-                    </a>
-                    <a href="#" class="nav-link">
-                        <p class="nav-link-text">About Us</p>
-                        <i class="eicon-caret-down arrow-down"></i>
-                    </a>
+                    <?php foreach ($menus as $menu) : ?>
+                        <a href="<?php echo $menu['url']; ?>" class="nav-link">
+                            <p class="nav-link-text"><?php echo $menu['title']; ?></p>
+                            <?php if (isset($menu['children'])) : ?>
+                                <i class="eicon-caret-down arrow-down"></i>
+                            <?php endif; ?>
+                        </a>
+                    <?php endforeach; ?>
                     <a href="#" class="nav-link login-button">
                         <p class="nav-link-text" <?php echo $this->get_render_attribute_string('sign_in_button_text'); ?>>
                             <?php echo $settings['sign_in_button_text']; ?>
