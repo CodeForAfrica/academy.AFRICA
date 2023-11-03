03f01f37d35a3b25324aa9feac44951b0923c28f
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-11-03 16:15:42 +0300

    basic theme setup
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- wp-content/themes/academyAfrica/functions.php
+++ wp-content/themes/academyAfrica/functions.php
@@ -1,24 +1,45 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+if (!function_exists('chld_thm_cfg_locale_css')) :
+    function chld_thm_cfg_locale_css($uri)
+    {
+        if (empty($uri) && is_rtl() && file_exists(get_template_directory() . '/rtl.css'))
             $uri = get_template_directory_uri() . '/rtl.css';
         return $uri;
     }
 endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
-         
-if ( !function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ctc-style.css', array( 'hello-elementor','hello-elementor','hello-elementor-theme-style' ) );
+add_filter('locale_stylesheet_uri', 'chld_thm_cfg_locale_css');
+
+if (!function_exists('child_theme_configurator_css')) :
+    function child_theme_configurator_css()
+    {
+        wp_enqueue_style('chld_thm_cfg_separate', trailingslashit(get_stylesheet_directory_uri()) . 'ctc-style.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'));
     }
 endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 10);
 
 // END ENQUEUE PARENT ACTION
+
+
+function my_theme_enqueue_styles()
+{
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/css/dist/main.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'));
+}
+
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+function my_theme_enqueue_scripts()
+{
+    $js_files = glob(get_stylesheet_directory() . '/assets/js/*.js');
+    foreach ($js_files as $js_file) {
+        $js_file_name = basename($js_file, '.js');
+        wp_enqueue_script($js_file_name, get_stylesheet_directory_uri() . '/assets/js/' . $js_file_name . '.js', [], true);
+    }
+}
+
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
---
+++ wp-content/themes/academyAfrica/template-parts/404.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @package Academy Africa
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+?>
+
+<?php get_header(); ?>
+
+<main id="content" class="site-main">
+
+    <?php if (apply_filters('academy_africa_page_title', true)) : ?>
+        <header class="page-header">
+            <h1 class="entry-title"><?php echo esc_html__('Looks like you\'re Lost again.', 'academy-africa'); ?></h1>
+        </header>
+    <?php endif; ?>
+
+    <div class="page-content">
+        <p><?php echo esc_html__('No page in Academy Africa.', 'academy-africa'); ?></p>
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
