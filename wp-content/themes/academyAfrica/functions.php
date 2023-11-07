ce1ff47f51685665155c55c898e3088a5fd6c239
Kevin Koech <koechkevin92@gmail.com>
2023-11-07 18:28:00 +0300

    Rename style

--- wp-content/themes/academyAfrica/functions.php
+++ wp-content/themes/academyAfrica/functions.php
@@ -31,7 +31,7 @@ add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 10);
 
 function my_theme_enqueue_styles()
 {
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/css/dist/main.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'));
+    wp_enqueue_style('renamed-style', get_stylesheet_directory_uri() . '/assets/css/dist/main.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'));
 }
 
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
