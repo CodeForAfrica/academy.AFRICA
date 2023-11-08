14557b679b1f9189dfac83fba5caf8cd4eeed385
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-11-08 09:40:27 +0300

    Fix header alignment

--- wp-content/themes/academyAfrica/template-parts/header.php
+++ wp-content/themes/academyAfrica/template-parts/header.php
@@ -82,10 +82,13 @@ $menu_items = MenuFunctions::get_menu_items('menu-1');
                         if (count($menu_item['children']) > 0) {
                             $class .= ' parent';
                             echo "<div class='" . $class . "'>";
-                            echo "<span class='collapsible'>" . $menu_item['title'] . "
-                    <i class='fa fa-chevron-down icon close'></i>
-                    <i class='fa fa-chevron-up icon open' style='display: none;'></i>
-                    </span>";
+                            echo 
+                            "<span class='collapsible'>" . $menu_item['title'] . "
+                                <div class='icons'>
+                                    <i class='fa fa-chevron-down icon close'></i>
+                                    <i class='fa fa-chevron-up icon open' style='display: none;'></i>
+                                </div>
+                            </span>";
                             echo "<div class='children'>";
                             foreach ($menu_item['children'] as $child) {
                                 echo "
