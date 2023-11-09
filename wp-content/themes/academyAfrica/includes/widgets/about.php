74f61e8ba80fcb09241088d811d32b4a13c80568
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-11-09 13:17:15 +0300

    Fix About section
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- wp-content/themes/academyAfrica/includes/widgets/about.php
+++ wp-content/themes/academyAfrica/includes/widgets/about.php
@@ -41,34 +41,6 @@ class Academy_Africa_About_Section extends \Elementor\Widget_Base
             ]
         );
         $this->add_control(
-            'header_image',
-            [
-                'label' => __('Header Image', 'academy-africa'),
-                'type' => \Elementor\Controls_Manager::MEDIA,
-                'default' => [
-                    'url' => \Elementor\Utils::get_placeholder_image_src(),
-                ],
-            ]
-        );
-        $this->add_control(
-            'title',
-            [
-                'label' => __('Title', 'academy-africa'),
-                'type' => \Elementor\Controls_Manager::TEXT,
-                'default' => __('academy.AFRICA', 'academy-africa'),
-                'label_block' => true,
-            ]
-        );
-        $this->add_control(
-            'headline',
-            [
-                'label' => __('Headline', 'academy-africa'),
-                'type' => \Elementor\Controls_Manager::TEXT,
-                'default' => __('Giving African newsrooms access to the world’s best digital experts and storytelling technologies.', 'academy-africa'),
-                'label_block' => true,
-            ]
-        );
-        $this->add_control(
             'content',
             [
                 'label' => __('Content', 'academy-africa'),
@@ -87,62 +59,9 @@ class Academy_Africa_About_Section extends \Elementor\Widget_Base
     protected function render()
     {
         $settings = $this->get_settings_for_display();
-        $title = $settings['title'];
         $content = $settings['content'];
-        $headline = $settings['headline'];
-        $header_image = $settings['header_image']['url'];
-        $social_media_links = [
-            [
-                'link' => [
-                    'url' => 'https://www.facebook.com/CodeForAfrica',
-                ],
-                'type' => 'facebook',
-            ],
-            [
-                'link' => [
-                    'url' => 'https://twitter.com/Code4Africa',
-                ],
-                'type' => 'twitter',
-            ],
-            [
-                'link' => [
-                    'url' => 'https://www.instagram.com/code4africa/',
-                ],
-                'type' => 'instagram',
-            ],
-            [
-                'link' => [
-                    'url' => 'https://www.linkedin.com/company/code-for-africa/',
-                ],
-                'type' => 'linkedin',
-            ],
-        ];
 ?>
         <div class="about-section">
-            <div class="about-hero">
-                <img src="<?php echo $header_image; ?>" alt="academy.AFRICA">
-            </div>
-            <div class="title">
-                <h1 class="cfa-title"><?php echo $title; ?></h1>
-            </div>
-            <div class="headline">
-                <h2 class="cfa-headline"><?php echo $headline; ?></h2>
-            </div>
-            <div class="share-menu">
-                <div class="social-icons">
-                    <?
-                    if (!empty($social_media_links)) {
-                        foreach ($social_media_links as $item) {
-                            $link = esc_url($item['link']['url']);
-                            $type = esc_html($item['type']);
-                            $icon = get_stylesheet_directory_uri() . ('/assets/images/icons/Type=' . $type . ', Size=24, Color=Black.svg');
-                            $image = "<img src='" . $icon . "' alt='" . $type . "' />";
-                            echo '<a style="color: #000" href="' . $link . '" class="icon">' . $image . '</a>';
-                        }
-                    }
-                    ?>
-                </div>
-            </div>
             <div class="content">
                 <?php echo $content; ?>
             </div>
