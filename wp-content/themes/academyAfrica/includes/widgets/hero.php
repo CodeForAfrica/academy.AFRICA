cdb6600d13a36ed8f8d037d9a87d066eb52ca4ca
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-11-16 13:36:54 +0300

    fix desktop small
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- wp-content/themes/academyAfrica/includes/widgets/hero.php
+++ wp-content/themes/academyAfrica/includes/widgets/hero.php
@@ -107,9 +107,10 @@ class Academy_Africa_Hero extends \Elementor\Widget_Base
         $settings = $this->get_settings_for_display();
         $title = $settings['title'];
         $sign_up_label = $settings['sign_up_label'];
-        $sign_up_url = $settings['sign_up_link']['url']; 
+        $sign_up_url = $settings['sign_up_link']['url'];
 ?>
         <div class="hero">
+            <div class="background-image"></div>
             <div class="content-parent">
                 <div class="content">
                     <div class="title">
@@ -143,36 +144,10 @@ class Academy_Africa_Hero extends \Elementor\Widget_Base
                         ?>
                     </div>
                 </div>
-                <!-- <img height="217" alt="mask" class="mask" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/mask.png' ?>"> -->
             </div>
             <div class="mask-image">
                 <img alt="mask" class="mask" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/mask.svg' ?>">
             </div>
-            <!-- <div class="content-parent">
-                <div class="metrics-content">
-                    <div class="metrics">
-                        <?
-                        if (!empty($settings['metrics'])) {
-                            foreach ($settings['metrics'] as $item) {
-                                $metric = esc_html($item['metric']);
-                                $label = esc_html($item['label']);
-                        ?>
-                                <div class="metric">
-                                    <h2 class="numbers">
-                                        <? echo $metric ?>
-                                    </h2>
-                                    <p class="label">
-                                        <? echo $label ?>
-                                    </p>
-                                </div>
-                        <?
-                            }
-                        }
-                        ?>
-                    </div>
-                </div>
-                <div class="mask"></div>
-            </div> -->
         </div>
 <?
     }
