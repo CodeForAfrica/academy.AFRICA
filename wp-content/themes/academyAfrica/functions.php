e78bf988a2a9d3549b27eb26788392ca1fab76b5
Kevin Koech <koechkevin92@gmail.com>
2023-12-01 12:15:27 +0300

    Fix sign up buttons

--- wp-content/themes/academyAfrica/functions.php
+++ wp-content/themes/academyAfrica/functions.php
@@ -31,7 +31,7 @@ add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 10);
 
 function my_theme_enqueue_styles()
 {
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/css/dist/main.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'), '6.3.13');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/css/dist/main.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'), '6.3.14');
     wp_enqueue_style('single-event', get_stylesheet_directory_uri() . '/assets/css/dist/pages/single_event.css', array(), '6.3.12');
     wp_enqueue_style('profile', get_stylesheet_directory_uri() . '/assets/css/dist/pages/profile.css', array(), '6.4');
     wp_enqueue_style('single-courses', get_stylesheet_directory_uri() . '/assets/css/dist/pages/single-sfwd-courses.css', array(), '6.3.12');
@@ -86,7 +86,7 @@ function create_organization_post_type()
             "heirarchical" => true,
             'rewrite' => array('slug' => 'academy-africa-organizations'),
             'show_in_rest' => true,
-            'supports' => array('title', 'thumbnail','editor', 'excerpt', 'custom-fields', 'revisions', 'page-attributes')
+            'supports' => array('title', 'thumbnail', 'editor', 'excerpt', 'custom-fields', 'revisions', 'page-attributes')
         )
     );
 }
