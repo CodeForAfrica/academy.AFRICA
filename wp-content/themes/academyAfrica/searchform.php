6df0bd5a588ec5720ccdd9b7fded275ff0889bbd
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-12-07 13:07:26 +0300

    Working search page
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

---
+++ wp-content/themes/academyAfrica/searchform.php
@@ -0,0 +1,8 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
+    <div class="input">
+        <input type="text" class="form-control" placeholder="Search Courses" aria-label="Search Courses" value="" name="s" id="s">
+        <button class="button primary medium" type="submit" id="searchsubmit">
+            <i class="fa-solid fa-magnifying-glass icon"></i>
+        </button>
+    </div>
+</form>
