ab7dd9fdef40cc031d0d0c088996d40aed7d2646
Kevin Koech <koechkevin92@gmail.com>
2024-01-04 14:19:12 +0300

    Fix footer style

--- wp-content/themes/academyAfrica/functions.php
+++ wp-content/themes/academyAfrica/functions.php
@@ -3,72 +3,79 @@
 // namespace AcademyAfrica\Theme;
 
 // Exit if accessed directly
-if(!defined('ABSPATH'))
+if (!defined('ABSPATH'))
     exit;
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if(!function_exists('chld_thm_cfg_locale_css')):
-    function chld_thm_cfg_locale_css($uri) {
-        if(empty($uri) && is_rtl() && file_exists(get_template_directory().'/rtl.css'))
-            $uri = get_template_directory_uri().'/rtl.css';
+if (!function_exists('chld_thm_cfg_locale_css')):
+    function chld_thm_cfg_locale_css($uri)
+    {
+        if (empty($uri) && is_rtl() && file_exists(get_template_directory() . '/rtl.css'))
+            $uri = get_template_directory_uri() . '/rtl.css';
         return $uri;
     }
 endif;
 add_filter('locale_stylesheet_uri', 'chld_thm_cfg_locale_css');
 
-if(!function_exists('child_theme_configurator_css')):
-    function child_theme_configurator_css() {
-        wp_enqueue_style('chld_thm_cfg_separate', trailingslashit(get_stylesheet_directory_uri()).'ctc-style.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'));
+if (!function_exists('child_theme_configurator_css')):
+    function child_theme_configurator_css()
+    {
+        wp_enqueue_style('chld_thm_cfg_separate', trailingslashit(get_stylesheet_directory_uri()) . 'ctc-style.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'));
     }
 endif;
 add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 10);
 
 // END ENQUEUE PARENT ACTION
 
-define('ACADEMY_AFRICA_VERSION', '1.1.9');
+define('ACADEMY_AFRICA_VERSION', '1.1.10');
 
-function my_theme_enqueue_styles() {
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/assets/css/dist/main.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style('single-event', get_stylesheet_directory_uri().'/assets/css/dist/pages/single_event.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style('profile', get_stylesheet_directory_uri().'/assets/css/dist/pages/profile.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style('single-courses', get_stylesheet_directory_uri().'/assets/css/dist/pages/single-sfwd-courses.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style('single-lesson', get_stylesheet_directory_uri().'/assets/css/dist/pages/single-sfwd-lessons.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style('single-quiz', get_stylesheet_directory_uri().'/assets/css/dist/pages/single-sfwd-quiz.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style('single-topic', get_stylesheet_directory_uri().'/assets/css/dist/pages/single-sfwd-topic.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style(('sfwd-common'), get_stylesheet_directory_uri().'/assets/css/dist/pages/sfwd-common.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style(('course-completed'), get_stylesheet_directory_uri().'/assets/css/dist/pages/course-completed.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style(('single-ac-learning-path'), get_stylesheet_directory_uri().'/assets/css/dist/pages/single-ac-learning-path.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style(('search'), get_stylesheet_directory_uri().'/assets/css/dist/pages/search.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style(('filter_bar'), get_stylesheet_directory_uri().'/assets/css/dist/pages/filter_bar.css', array(), ACADEMY_AFRICA_VERSION);
+function my_theme_enqueue_styles()
+{
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/css/dist/main.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style'), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style('single-event', get_stylesheet_directory_uri() . '/assets/css/dist/pages/single_event.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style('profile', get_stylesheet_directory_uri() . '/assets/css/dist/pages/profile.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style('single-courses', get_stylesheet_directory_uri() . '/assets/css/dist/pages/single-sfwd-courses.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style('single-lesson', get_stylesheet_directory_uri() . '/assets/css/dist/pages/single-sfwd-lessons.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style('single-quiz', get_stylesheet_directory_uri() . '/assets/css/dist/pages/single-sfwd-quiz.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style('single-topic', get_stylesheet_directory_uri() . '/assets/css/dist/pages/single-sfwd-topic.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style(('sfwd-common'), get_stylesheet_directory_uri() . '/assets/css/dist/pages/sfwd-common.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style(('course-completed'), get_stylesheet_directory_uri() . '/assets/css/dist/pages/course-completed.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style(('single-ac-learning-path'), get_stylesheet_directory_uri() . '/assets/css/dist/pages/single-ac-learning-path.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style(('search'), get_stylesheet_directory_uri() . '/assets/css/dist/pages/search.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style(('filter_bar'), get_stylesheet_directory_uri() . '/assets/css/dist/pages/filter_bar.css', array(), ACADEMY_AFRICA_VERSION);
 }
 
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 
-function load_admin_styles() {
-    wp_enqueue_style('event-style', get_stylesheet_directory_uri().'/assets/css/dist/admin/events.css', array(), ACADEMY_AFRICA_VERSION);
-    wp_enqueue_style('main-style', get_stylesheet_directory_uri().'/assets/css/dist/admin/main.css', array(), ACADEMY_AFRICA_VERSION);
+function load_admin_styles()
+{
+    wp_enqueue_style('event-style', get_stylesheet_directory_uri() . '/assets/css/dist/admin/events.css', array(), ACADEMY_AFRICA_VERSION);
+    wp_enqueue_style('main-style', get_stylesheet_directory_uri() . '/assets/css/dist/admin/main.css', array(), ACADEMY_AFRICA_VERSION);
 }
 add_action('admin_enqueue_scripts', 'load_admin_styles');
 
-function load_fa() {
+function load_fa()
+{
     wp_enqueue_style('load-fa', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css');
 }
 
 add_action('wp_enqueue_scripts', 'load_fa');
 
-function my_theme_enqueue_scripts() {
-    $js_files = glob(get_stylesheet_directory().'/assets/js/*.js');
-    foreach($js_files as $js_file) {
+function my_theme_enqueue_scripts()
+{
+    $js_files = glob(get_stylesheet_directory() . '/assets/js/*.js');
+    foreach ($js_files as $js_file) {
         $js_file_name = basename($js_file, '.js');
-        wp_enqueue_script($js_file_name, get_stylesheet_directory_uri().'/assets/js/'.$js_file_name.'.js', [], ACADEMY_AFRICA_VERSION);
+        wp_enqueue_script($js_file_name, get_stylesheet_directory_uri() . '/assets/js/' . $js_file_name . '.js', [], ACADEMY_AFRICA_VERSION);
     }
     wp_enqueue_script("canvas", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js", array(), ACADEMY_AFRICA_VERSION);
     wp_enqueue_script("jsPDF", "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js", array(), ACADEMY_AFRICA_VERSION);
 }
 
-function home_page() {
+function home_page()
+{
     return '/';
 }
 
@@ -78,7 +85,8 @@ add_filter('logout_redirect', 'home_page');
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
 
 // Organizations Post Type
-function create_organization_post_type() {
+function create_organization_post_type()
+{
     register_post_type(
         'ac-organization',
         array(
@@ -97,7 +105,8 @@ function create_organization_post_type() {
 }
 add_action('init', 'create_organization_post_type');
 
-function create_learning_path_post_type() {
+function create_learning_path_post_type()
+{
     register_post_type(
         'ac-learning-path',
         array(
@@ -116,17 +125,18 @@ function create_learning_path_post_type() {
 }
 add_action('init', 'create_learning_path_post_type');
 
-require_once __DIR__.'/includes/widgets/widgets.php';
+require_once __DIR__ . '/includes/widgets/widgets.php';
 $widget = new \AcademyAfrica\Theme\Widget\Widget();
 $widget->init();
 
-require_once __DIR__.'/posts/events.php';
-require_once __DIR__.'/posts/networks.php';
+require_once __DIR__ . '/posts/events.php';
+require_once __DIR__ . '/posts/networks.php';
 add_action('init', 'event_post_type');
 add_action('init', 'create_networks_post_type');
 
-function redirect_to_custom_profile_edit() {
-    if(is_user_logged_in() && is_admin() && basename($_SERVER['PHP_SELF']) == 'profile.php') {
+function redirect_to_custom_profile_edit()
+{
+    if (is_user_logged_in() && is_admin() && basename($_SERVER['PHP_SELF']) == 'profile.php') {
         $custom_profile_edit_url = home_url('/profile');
         wp_redirect($custom_profile_edit_url);
         exit();
@@ -137,10 +147,11 @@ add_action('template_redirect', 'redirect_to_custom_profile_edit');
 
 add_filter('get_avatar_data', 'change_avatar', 100, 2);
 
-function change_avatar($args, $id_or_email) {
+function change_avatar($args, $id_or_email)
+{
     $avatar_url = get_user_meta($id_or_email, 'avatar', true);
 
-    if(!empty($avatar_url)) {
+    if (!empty($avatar_url)) {
         $args['url'] = $avatar_url;
     }
     return $args;
@@ -148,19 +159,21 @@ function change_avatar($args, $id_or_email) {
 
 add_filter('acf/settings/show_admin', 'my_acf_show_admin');
 
-function my_acf_show_admin($show) {
+function my_acf_show_admin($show)
+{
 
     return current_user_can('manage_options');
 }
 
 add_action('user_register', 'send_activation_link', 10, 1);
 
-function send_activation_link($user_id) {
-    if($user_id) {
+function send_activation_link($user_id)
+{
+    if ($user_id) {
         $user = get_user_by('ID', $user_id);
-        $sign_in_url = home_url().'#sign-in';
+        $sign_in_url = home_url() . '#sign-in';
         $code = $user->data->user_activation_key;
-        $valid_code = isset($code) ? $code : sha1($user_id.time());
+        $valid_code = isset($code) ? $code : sha1($user_id . time());
         global $wpdb;
         $wpdb->update(
             'wp_users',
@@ -174,11 +187,12 @@ function send_activation_link($user_id) {
             console.log(<? echo json_encode($user) ?>, <? echo json_encode($activation_link) ?>);
         </script>
         <?
-        wp_mail($email, '[academy.AFRICA] Login Details', 'Activation link : '.$activation_link);
+        wp_mail($email, '[academy.AFRICA] Login Details', 'Activation link : ' . $activation_link);
     }
 }
 
-function whitelist_address() {
+function whitelist_address()
+{
     return array(
         '127.0.0.1',
         '::1',
@@ -186,14 +200,15 @@ function whitelist_address() {
     );
 }
 
-function restrict_user_status($user, $username, $password) {
-    if(!in_array($_SERVER['REMOTE_ADDR'], whitelist_address())) {
+function restrict_user_status($user, $username, $password)
+{
+    if (!in_array($_SERVER['REMOTE_ADDR'], whitelist_address())) {
         return $user;
     } else {
-        if($user instanceof WP_User) {
+        if ($user instanceof WP_User) {
             $account_status = get_user_meta($user->data->ID, 'account_status', true);
-            if($user->data->user_status == 1 || $account_status !== "active") {
-                if(!isset($user->data->user_activation_key)) {
+            if ($user->data->user_status == 1 || $account_status !== "active") {
+                if (!isset($user->data->user_activation_key)) {
                     send_activation_link($user->data->ID);
                 }
                 return new WP_Error('authentication_failed', __('<strong>ERROR</strong>: Your account is not active.'));
@@ -206,17 +221,18 @@ function restrict_user_status($user, $username, $password) {
     }
 }
 
-function authenticate_user() {
+function authenticate_user()
+{
     $user_id = get_current_user_id();
     $user = get_user_by('ID', $user_id);
-    if(!in_array($_SERVER['REMOTE_ADDR'], whitelist_address())) {
+    if (!in_array($_SERVER['REMOTE_ADDR'], whitelist_address())) {
         return $user;
     } else {
-        if($user instanceof WP_User) {
+        if ($user instanceof WP_User) {
             $account_status = get_user_meta($user->data->ID, 'account_status', true);
             global $whitelist;
-            if(!in_array($_SERVER['REMOTE_ADDR'], whitelist_address())) {
-                if($user->data->user_status == 1 || $account_status !== "active") {
+            if (!in_array($_SERVER['REMOTE_ADDR'], whitelist_address())) {
+                if ($user->data->user_status == 1 || $account_status !== "active") {
                     send_activation_link($user->data->ID);
                     wp_logout();
                     ?>
--- wp-content/themes/academyAfrica/template-parts/footer.php
+++ wp-content/themes/academyAfrica/template-parts/footer.php
@@ -61,15 +61,18 @@ $secondary_menus = [
 ]
 
 
-?>
+    ?>
 
-<footer>
+<footer class="footer-wrapper">
     <div class="root">
         <div class="item">
             <div class="site-description">
-                <img height="110" width="250" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/cfa_logo.svg' ?>" alt=<?php echo get_bloginfo('name'); ?> class="logo">
+                <img height="110" width="250"
+                    src="<?php echo get_stylesheet_directory_uri() . '/assets/images/cfa_logo.svg' ?>" alt=<?php echo get_bloginfo('name'); ?> class="logo">
                 <p class="description">
-                    This site is a project of Code for Africa, the continent's largest network of civic technology and data journalism labs. All content is released under a Creative Commons Attribution Licence. Reuse it to help empower your own community.
+                    This site is a project of Code for Africa, the continent's largest network of civic technology and
+                    data journalism labs. All content is released under a Creative Commons Attribution Licence. Reuse it
+                    to help empower your own community.
                 </p>
                 <div class="footer-connect">
                     <span style="white-space: nowrap;">
@@ -119,15 +122,21 @@ $secondary_menus = [
                 </p>
                 <div>
                     <div id="mc_embed_signup">
-                        <form action="https://twitter.us6.list-manage.com/subscribe/post?u=65e5825507b3cec760f272e79&amp;id=c2ff751541" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+                        <form
+                            action="https://twitter.us6.list-manage.com/subscribe/post?u=65e5825507b3cec760f272e79&amp;id=c2ff751541"
+                            method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
+                            class="validate" target="_blank" novalidate="">
                             <div id="mc_embed_signup_scroll">
                                 <label for="MERGE1">Name</label>
                                 <input type="text" name="MERGE1" id="MERGE1" size="25" value="" placeholder="Your name">
                                 <label for="mce-EMAIL">Email</label>
-                                <input type="email" value="" placeholder="example@email.com" name="EMAIL" class="email" id="mce-EMAIL" required="">
+                                <input type="email" value="" placeholder="example@email.com" name="EMAIL" class="email"
+                                    id="mce-EMAIL" required="">
                                 <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_65e5825507b3cec760f272e79_c2ff751541" tabindex="-1" value=""></div>
-                                <div class="clear"><input type="submit" value="Sign up" id="mc-embedded-subscribe" class="button"></div>
+                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
+                                        name="b_65e5825507b3cec760f272e79_c2ff751541" tabindex="-1" value=""></div>
+                                <div class="clear"><input type="submit" value="Sign up" id="mc-embedded-subscribe"
+                                        class="button"></div>
                             </div>
                         </form>
                     </div>
