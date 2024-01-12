61cf18e0a3cc9c235f5f0c60bb6542c74bd4c7a2
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-01-12 19:20:34 +0300

    PDF Download
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- wp-content/themes/academyAfrica/single-ac-learning-path.php
+++ wp-content/themes/academyAfrica/single-ac-learning-path.php
@@ -84,6 +84,79 @@ $courses = get_field('courses', $learning_path_id);
             </div>
         </div>
     </div>
+    <div id="learning">
+        <div class="pdf-template">
+            <div class="template">
+                <div class="template__title">
+                    <div class="template__title__image">
+                        <img src="/wp-content/themes/academyAfrica/assets/images/mask.svg" class="image">
+                    </div>
+                    <div class="template__title__text">
+                        <div class="cfa-title">
+                            <?php the_title(); ?>
+                        </div>
+                        <div class="cfa-excerpt">
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="template__content">
+                    <div class="courses">
+                        <div class="title">
+                            Take the courses
+                        </div>
+                        <div class="content">
+                            <? the_content() ?>
+                        </div>
+                        <div class="list">
+                            <?php foreach ($courses as $course_index =>  $course) : ?>
+                                <?
+                                $course_thumbnail = get_the_post_thumbnail_url($course);
+                                $course_title = $course->post_title;
+                                $course_excerpt = $course->post_excerpt;
+                                $course_link = get_permalink($course->ID);
+                                $course_author = get_the_author_meta('display_name', $course->post_author);
+                                $course_meta = get_post_meta($course->ID);
+                                $course_price = $course_meta['sfwd-courses_course_price'];
+                                $course_price = $course_price == 0 ? "Free" : $course_price;
+                                $students_count = learndash_course_grid_count_students($course->ID);
+                                ?>
+                                <div class="individual-course">
+                                    <div class="course-index">
+                                        <? echo $course_index + 1 ?>
+                                    </div>
+                                    <div class="course">
+                                        <?php get_template_part(
+                                            'template-parts/course_card',
+                                            'template',
+                                            [
+                                                'course_title' => $course_title,
+                                                'course_author' => $course_author,
+                                                'course_thumbnail' => $course_thumbnail,
+                                                'course_link' => $course_link,
+                                                'course_price' => $course_price,
+                                                'students' => $students_count
+                                            ]
+                                        ); ?>
+                                        <div class="extra-course-details">
+                                            <a href="<? echo $course_link ?>" class="course-title">
+                                                <? echo $course_title ?>
+                                            </a>
+                                            <div class="course-excerpt">
+                                                <? echo $course_excerpt ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr class="divider">
+                            <?php endforeach; ?>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="ac-learning-path-container__download">
         <button class="button primary large" onclick="downloadSingleLearningPath()">
             <i class="fa-solid fa-download icon"></i>
@@ -98,26 +171,27 @@ $courses = get_field('courses', $learning_path_id);
             jsPDF
         } = window.jspdf;
 
+
+        let pdf = document.getElementById('learning');
+        let pdfTemplate = pdf.querySelector('.pdf-template');
+
+        const width = pdfTemplate.offsetWidth;
+        const height = pdfTemplate.offsetHeight;
+
         let doc = new jsPDF({
             orientation: 'p',
             unit: 'px',
-            format: 'a4',
+            format: [width, parseInt(height) + 100],
             putOnlyUsedFonts: true,
         });
-        let pdf = document.getElementById('learning-path');
-
-        const width = pdf.offsetWidth;
-        const height = pdf.offsetHeight;
-        doc.html(pdf, {
+        doc.html(pdfTemplate, {
             callback: function(doc) {
                 doc.save(`<? echo $learning_path_title . '.pdf' ?>`);
             },
-            margin: 0.5,
-            autoPaging: true,
+            margin: 0,
             html2canvas: {
                 scale: 1
             },
-            width: 1920,
         });
     }
 </script>
