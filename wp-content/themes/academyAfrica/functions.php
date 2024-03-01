547d9dc1bd8aecc70824eea241b98b47da81426c
Kevin Koech <koechkevin92@gmail.com>
2024-03-01 16:56:55 +0300

    Bump version

--- wp-content/themes/academyAfrica/functions.php
+++ wp-content/themes/academyAfrica/functions.php
@@ -29,7 +29,7 @@ add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 10);
 
 // END ENQUEUE PARENT ACTION
 
-define('ACADEMY_AFRICA_VERSION', '1.1.30');
+define('ACADEMY_AFRICA_VERSION', '1.1.31');
 const MINIMUM_ELEMENTOR_VERSION = '3.16.6';
 
 
