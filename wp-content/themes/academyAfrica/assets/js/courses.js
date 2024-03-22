cd9e2fc2fb925915f50128de22d0bb914446e891
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-03-22 09:56:27 +0300

    Fix Enroll BTN
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- wp-content/themes/academyAfrica/assets/js/courses.js
+++ wp-content/themes/academyAfrica/assets/js/courses.js
@@ -1,12 +1,12 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const enrollBtn = document.querySelectorAll("#enroll-button");
-    if(enrollBtn.length){
-        enrollBtn.forEach(btn => {
-        const btnSubmit = btn.querySelector("input[type='submit'].learndash-button-free");
-        if(btnSubmit){
-            btnSubmit.value = "Enroll Now";
-            btnSubmit.classList.add("button", "secondary", "large");
-        }
-    });
-    }
- });
+// window.addEventListener("DOMContentLoaded", () => {
+//     const enrollBtn = document.querySelectorAll("#enroll-button");
+//     if(enrollBtn.length){
+//         enrollBtn.forEach(btn => {
+//         const btnSubmit = btn.querySelector("input[type='submit'].learndash-button-free");
+//         if(btnSubmit){
+//             btnSubmit.value = "Enroll Now";
+//             btnSubmit.classList.add("button", "secondary", "large");
+//         }
+//     });
+//     }
+//  });
