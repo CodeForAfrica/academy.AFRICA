9b12e0e34f2944cfd5dc9d69d8c2cf445d758a5f
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-04-24 10:25:39 +0300

    Fix broken lesson issue
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- wp-content/themes/academyAfrica/functions.php
+++ wp-content/themes/academyAfrica/functions.php
@@ -29,7 +29,7 @@ add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 10);
 
 // END ENQUEUE PARENT ACTION
 
-define('ACADEMY_AFRICA_VERSION', '1.1.89');
+define('ACADEMY_AFRICA_VERSION', '1.1.90');
 const MINIMUM_ELEMENTOR_VERSION = '3.16.6';
 
 
