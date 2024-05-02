3a2f15b346ea07e6e7db54a3d6b5f6a141b9d0cd
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-05-02 15:55:05 +0300

    Fix Quiz complete

--- wp-content/themes/academyAfrica/assets/js/courses.js
+++ wp-content/themes/academyAfrica/assets/js/courses.js
@@ -1,19 +1,20 @@
 window.addEventListener("DOMContentLoaded", () => {
     const enrollBtn = document.querySelectorAll("#enroll-button");
-    if(enrollBtn.length){
+    if (enrollBtn.length) {
         enrollBtn.forEach(btn => {
-        const btnSubmit = btn.querySelector("input[type='submit'].learndash-button-free");
-        if(btnSubmit){
-            btnSubmit.value = "Enroll Now";
-            btnSubmit.classList.add("button", "secondary", "large");
-        }
-    });
+            const btnSubmit = btn.querySelector("input[type='submit'].learndash-button-free");
+            if (btnSubmit) {
+                btnSubmit.value = "Enroll Now";
+                btnSubmit.classList.add("button", "secondary", "large");
+            }
+        });
     }
 
-    // get button with name 'reShowQuestion' and class 'wpProQuiz_button_reShowQuestion'
-    const reShowQuestion = document.querySelector("button[name='reShowQuestion'].wpProQuiz_button_reShowQuestion");
-    if(reShowQuestion){
-    //    console.log("reShowQuestion button found");
-    //    TODO: update inner text to View Answers
-    }
- });
+    setInterval(() => {
+        const reShowQuestion = document.querySelector("input[name='reShowQuestion'].wpProQuiz_button_reShowQuestion");
+        if (reShowQuestion) {
+            reShowQuestion.value = "View Answers";
+        }
+    }, 1000)
+});
+
