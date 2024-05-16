4f5bb9a13d8ac8e1316b2e702c377010441d6065
Kevin Koech <koechkevin92@gmail.com>
2024-05-16 15:30:38 +0300

    Fix share icons

--- wp-content/themes/academyAfrica/template-parts/social_share.php
+++ wp-content/themes/academyAfrica/template-parts/social_share.php
@@ -1,29 +1,32 @@
 <?php
+function get_image($type){
+    return get_stylesheet_directory_uri() . ('/assets/images/icons/Type=' . $type . ', Size=24, Color=Black.svg');
+}
 $url_to_share = isset($args["url"]) ? $args["url"] : get_permalink(0);
 ?>
 <div style="display: flex;" class="share-menu">
 <div style="color: #000; cursor: pointer; margin-right: 16px;" class="share-icon" onclick="toggleShareButtons()">
-        <img class='icon-image' src="/assets/images/icons/Type=share, Size=24, Color=Black.svg" alt="Share">
+        <img class='icon-image' src="<? echo get_stylesheet_directory_uri() ?>/assets/images/icons/Type=share, Size=24, Color=Black.svg" alt="Share">
     </div>
     <div class="share-icons hide">
         <!-- LinkedIn -->
         <a style="color: #000; margin-right: 8px;" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $url_to_share; ?>" target="_blank">
-            <img class='icon-image' src="/assets/images/icons/Type=linkedin, Size=24, Color=Black.svg" alt="LinkedIn">
+            <img class='icon-image' src="<? echo get_stylesheet_directory_uri() ?>/assets/images/icons/Type=linkedin, Size=24, Color=Black.svg" alt="LinkedIn">
         </a>
         
         <!-- Twitter -->
         <a style="color: #000; margin-right: 8px;" href="https://twitter.com/intent/tweet?url=<?php echo $url_to_share; ?>" target="_blank">
-            <img class='icon-image' src="/assets/images/icons/Type=twitter, Size=24, Color=Black.svg" alt="Twitter">
+            <img class='icon-image' src="<? echo get_stylesheet_directory_uri() ?>/assets/images/icons/Type=twitter, Size=24, Color=Black.svg" alt="Twitter">
         </a>
         
         <!-- Facebook -->
         <a style="color: #000; margin-right: 8px;" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url_to_share; ?>" target="_blank">
-            <img class='icon-image' src="/assets/images/icons/Type=facebook, Size=24, Color=Black.svg" alt="Facebook">
+            <img class='icon-image' src="<? echo get_stylesheet_directory_uri() ?>/assets/images/icons/Type=facebook, Size=24, Color=Black.svg" alt="Facebook">
         </a>
         
         <!-- Instagram -->
         <a style="color: #000; margin-right: 8px;" href="https://www.instagram.com/" target="_blank">
-            <img class='icon-image' src="/assets/images/icons/Type=instagram, Size=24, Color=Black.svg" alt="Instagram">
+            <img class='icon-image' src="<? echo get_stylesheet_directory_uri() ?>/assets/images/icons/Type=instagram, Size=24, Color=Black.svg" alt="Instagram">
         </a>
     </div>
 </div>
