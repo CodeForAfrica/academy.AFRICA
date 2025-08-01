4d869f111300273dd98021db3d3c5b273847b9ee
Kevin Koech <koechkevin92@gmail.com>
2025-08-01 12:45:02 +0300

    Refactor page content template structure and update main class names

--- wp-content/themes/academyAfrica/page-content.php
+++ wp-content/themes/academyAfrica/page-content.php
@@ -6,17 +6,19 @@ require_once(ABSPATH . 'wp-load.php');
 get_header();
 
 ?>
-<main class="default-page-content">
-    <?php
-    if (have_posts()) {
-        while (have_posts()) {
-            the_post();
-            the_content();
+<main class="about-section">
+    <div class="content">
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                the_content();
+            }
+        } else {
+            echo '<p>No content available.</p>';
         }
-    } else {
-        echo '<p>No content available.</p>';
-    }
-    ?>
+        ?>
+    </div>
 </main>
 <?php
 get_footer();
