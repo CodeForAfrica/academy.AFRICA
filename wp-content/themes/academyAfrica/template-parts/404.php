540b646f4ef88aaf8819b75796e65de718f5b1f4
Kevin Koech <kevin@kevinkoech.com>
2025-12-10 10:33:56 +0300

    Fix title casing for 404 page title

--- wp-content/themes/academyAfrica/template-parts/404.php
+++ wp-content/themes/academyAfrica/template-parts/404.php
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
 <?php get_header(); ?>
 <?
 $status_code = 404;
-$title = "Page not found -";
+$title = "PAGE NOT FOUND";
 $description = "There seems to be an error on this page. Please contact us for more details";
 $refresh = "Refresh";
 $home = "Home";
